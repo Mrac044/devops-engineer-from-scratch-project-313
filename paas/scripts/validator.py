985378cb96f45d47a7fc39b3fa850eb8a88a6778
Mrac044 <doctor.06@bk.ru>
2026-05-29 00:00:54 +0700

    fix validating data

--- paas/scripts/validator.py
+++ paas/scripts/validator.py
@@ -1,4 +1,8 @@
 def validate(link):
+
+    if not isinstance(link, dict):
+        return {"payload": "Invalid data format"}
+
     errors = {}
     if not link.get('original_url'):
         errors['original_url'] = "Can't be blank"
